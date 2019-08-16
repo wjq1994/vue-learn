c0d073b29bd2ee2fdf576041237425b3ab4fdbd9
wangjianqing <wangjianqing@ktjr.com>
2019-08-16 18:42:00 +0800

    提交插件lodash

---
+++ my-project/src/plugins/lodash/lodash-map.vue
@@ -0,0 +1,45 @@
+<template>
+</template>
+
+<script>
+	import {
+		map
+	} from 'lodash'
+
+	export default {
+		name: 'LodashMap',
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			square(value, key, collection) {
+				// window.console.log(value)
+				// window.console.log(key)
+				// window.console.log(collection)
+				return value * value;
+			}
+		},
+		created() {
+			// window.console.log(map({
+			// 	'a': 4,
+			// 	'b': 8
+			// }, this.square));
+			// window.console.log(map({
+			// 	'a': 4,
+			// 	'b': 8
+			// }, 'a'));
+			window.console.log(map({
+				'a': {
+					'c': 66,
+					'd': 77
+				},
+				'b': 88
+			}, (value, key, collection) => collection));
+		}
+	}
+</script>
+
+<style>
+</style>
--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,18 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../views/Login.vue'
+import Map from '../plugins/lodash/lodash-map.vue'
 
 Vue.use(Router)
 const router = new Router({
   //mode属性，默认hash模式 http://localhost:8081/#/login
-  //mode: 'history',
+  mode: 'history',
   routes: [{
     path: '/',
     redirect: '/login'
-  },,{
+  },{
     path: '/login',
     name: 'Login',
     component: Login
+  },{
+    path: '/map',
+    name: 'LodashMap',
+    component: Map
   }]
 })
 
