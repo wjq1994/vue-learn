d1690aeca171955061a71ede8f6e3997b29e6538
wangjianqing <wangjianqing@ktjr.com>
2019-10-31 19:06:00 +0800

    新增computer

---
+++ my-project/src/views/grammar/computer.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<div>{{now}}</div>
+		<div id="example">
+		  <p>Original count: "{{ originCnt }}"</p>
+		  <p>Computed reversed count: "{{ reversedMessage }}"</p>
+		  <button @click="add">+1</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				originCnt: 1,
+				count: 1
+			}
+		},
+		computed: {
+			//Date.now()不是响应式的, now值不会变
+			now: function() {
+				return Date.now()
+			},
+			//计算属性是基于它们的响应式依赖进行缓存的  this.count就是响应式的
+			reversedMessage(newValue, oldValue) {
+				return this.count
+			}
+		},
+		methods: {
+			add() {
+				this.count++
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
